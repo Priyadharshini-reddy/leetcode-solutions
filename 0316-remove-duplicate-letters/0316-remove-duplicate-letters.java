5e9f774ce0b900a9e0e3011e686264c3b29bc817
Priyadharshini-reddy <priyadarshinireddy51@gmail.com>
2026-09-14 18:24:06 +0530

    Time: 5 ms (8.3%), Space: 45 MB (5.92%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        
+        HashMap<Character,Integer> map=new HashMap<>();
+        Stack<Character> s1=new Stack<>();
+         HashSet<Character> inStack = new HashSet<>();
+        
+   for(char c : s.toCharArray()){
+    map.put(c, map.getOrDefault(c, 0) + 1);
+}
+
+for(char c :s.toCharArray()){
+
+  map.put(c,map.get(c)-1);
+  if(inStack.contains(c)){
+    continue;
+  }
+
+  while(!s1.isEmpty() && s1.peek()>c && map.get(s1.peek())>0){
+     inStack.remove(s1.peek());
+    s1.pop();
+  }
+   s1.push(c);
+    inStack.add(c);
+
+
+
+}
+ StringBuilder ans = new StringBuilder();
+
+        for (char c : s1) {
+            ans.append(c);
+        }
+
+        return ans.toString();
+
+
+    }
+}
+/*
+greedy+ stack
+
+it cannot have a next smaller 
+condition is that if it i mean go  through the chararray 
+and check if something 
+i mean if it is  not satisfying the order and occurs later pop it 
+if not satisfying and not occurs let it be 
+
+if it is satisfying but alredy taken into stack then skip 
+
+ */
