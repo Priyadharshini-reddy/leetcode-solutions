10a35719580146c18e68c5b012ef4d23749f6e37
Priyadharshini-reddy <priyadarshinireddy51@gmail.com>
2026-09-17 23:46:44 +0530

    Time: 16 ms (89.94%), Space: 48.6 MB (63.87%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        inorder(root1, list1);
+        inorder(root2, list2);
+
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                result.add(list1.get(i++));
+            } else {
+                result.add(list2.get(j++));
+            }
+        }
+
+        while (i < list1.size()) {
+            result.add(list1.get(i++));
+        }
+
+        while (j < list2.size()) {
+            result.add(list2.get(j++));
+        }
+
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+}
+/**
+compare both the lefts and go for 
+
+ */
